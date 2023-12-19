1802667df653e421b4dea71e99cc0180e36ab324
HoangLeAnhTuan <hoangleanhtuannk@gmail.com>
2023-12-20 01:14:13 +0700

    chat function

---
+++ lib/view/customer/customer_home/customer_root_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:three_tapp_app/view/common_view/chat/chat_screen.dart';
+
+import '../blogs/blogs_screen.dart';
+import 'customer_home_screen.dart';
+
+class CustomerRootScreen extends StatefulWidget {
+  const CustomerRootScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _CustomerRootScreen();
+}
+
+class _CustomerRootScreen extends State<CustomerRootScreen> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    CustomerHomeScreen(),
+    BlogsScreen(),
+    ChatScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void reloadView() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: _widgetOptions.elementAt(_selectedIndex),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Trang chủ',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.note_rounded),
+              label: 'Bài viết',
+            ),
+                        BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: 'Chat',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.lightBlue,
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+}
